4366f64aaee314ca6c921716ad0faf635f675129
baijal1982 <elearning.amit1982@gmail.com>
2021-08-23 07:54:04 +0700

    adding find peek value code for 1 dim

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod peek_finding;
+
 fn main() {
-    println!("Hello, world!");
+    let data= vec![3,5,6,8,9,2];
+    let length = data.len();
+    peek_finding::peek::find_peek(data,0,length-1,length);
 }
---
+++ src/peek_finding/peek.rs
@@ -0,0 +1,22 @@
+
+// this method will find a peek in the input array using binary search 
+pub fn find_peek( data : Vec<u32>, initial_value: usize, final_value: usize , length :usize )   {
+
+
+   
+     let mid = initial_value + (final_value - initial_value)/2;
+
+     // best case if mid value  is peek
+     if (initial_value == 0 || data[mid] > data[mid-1]) && (final_value== length || data[ mid] >  data[ mid+1] )  {
+        println!("the peek value {} exist at index {}", data[ mid] , mid);
+        return;
+     }  // searching towards right 
+       else  if mid < data.len()-1  && data[mid+1] > data[mid]  {
+        find_peek(data, mid+1, final_value,length);
+     } 
+     // searching towards left
+     else {
+        find_peek(data, initial_value, mid,length);
+    } 
+     }
+
