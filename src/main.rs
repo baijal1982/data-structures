e7c3d38f9a6bf477aa0df7c889464359d94f539a
baijal1982 <elearning.amit1982@gmail.com>
2021-08-23 22:15:26 +0700

    insertion sort

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod peek_finding;
+mod sorting;
 
 fn main() {
     let data= vec![3,5,6,8,9,2];
     let length = data.len();
     peek_finding::peek::find_peek(data,0,length-1,length);
 }
+ 
---
+++ src/sorting/insertion_sort.rs
@@ -0,0 +1,18 @@
+
+
+//insertion sort in ascending order 
+pub fn sort( mut data : Vec<i64>)  ->  Vec<i64>   {
+for j in 1..data.len() {
+
+    let key =  data[j];
+       let mut i=  j;
+
+    while  i>0 && data[i-1]> key   {
+           data[i]= data[i-1];
+         i-=1;
+       }
+       data[i+1] = key;
+     
+    }
+data
+}
