03658a48c0518b252e509c85dfcb820291ae06d1
baijal1982 <elearning.amit1982@gmail.com>
2021-09-02 08:00:25 +0700

    formatting

--- src/basics/arrays/shuffle.rs
+++ src/basics/arrays/shuffle.rs
@@ -4,25 +4,25 @@
 
 //Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 // This runs in time complexity O(n/2) and spsace complexity O(n)
-pub fn shuffle(array:&mut Vec<i32>,n:u32) -> Vec<i32>  {
+pub fn shuffle(array:Vec<i32>,n:u32) -> Vec<i32>  {
     
     if n <3 {
         return  array.to_vec();
     }
-    let mut newArray = vec![0;n as usize];
+    let mut new_array = vec![0;n as usize];
 
     if n%2 !=0 {
-        newArray[(n-1) as usize] = array[(n-1) as usize];
+        new_array[(n-1) as usize] = array[(n-1) as usize];
     }
 
     let mid= n/2;
     for i in 0..mid {
-          newArray[(i*2) as usize] = array[i as usize];
+        new_array[(i*2) as usize] = array[i as usize];
           let other_mid_index = mid +i;
-          newArray[(other_mid_index-(mid-(i+1))) as usize] = array[other_mid_index as usize];
+          new_array[(other_mid_index-(mid-(i+1))) as usize] = array[other_mid_index as usize];
     }
 
-   newArray
+    new_array
 }
 
 
@@ -31,28 +31,28 @@ mod tests {
     use super::*;
     #[test]
     fn test_even_elements() {
-        let mut data= vec![2,5,1,3,4,7];
+        let  data= vec![2,5,1,3,4,7];
         let expected= vec![2,3,5,4,1,7];
-        assert_eq!( expected, shuffle(&mut data,6));
+        assert_eq!( expected, shuffle(data,6));
     }
     #[test]
     fn test_odd_elements() {
-        let mut data= vec![2,5,1,3,4];
+        let  data= vec![2,5,1,3,4];
         let expected= vec![2,1,5,3,4];
-        assert_eq!( expected, shuffle(&mut data,5));
+        assert_eq!( expected, shuffle(data,5));
     }
     #[test]
     fn test_single_element() {
-        let mut data= vec![10];
+        let  data= vec![10];
         let expected= vec![10];
-        assert_eq!( expected, shuffle(&mut data,1));
+        assert_eq!( expected, shuffle(data,1));
     }
 
     #[test]
     fn test_negative_elements() {
-        let mut data= vec![-2,5,-1,3,4,-7];
+        let  data= vec![-2,5,-1,3,4,-7];
         let expected= vec![-2,3,5,4,-1,-7];
-        assert_eq!( expected, shuffle(&mut data,6));
+        assert_eq!( expected, shuffle(data,6));
     }
 
 
