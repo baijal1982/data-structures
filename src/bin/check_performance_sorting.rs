b1aec6233eea759220d8d3e1ef2fd80dba35b325
baijal1982 <elearning.amit1982@gmail.com>
2021-12-09 08:02:30 +0700

    sorting perf

--- src/bin/check_performance_sorting.rs
+++ src/bin/check_performance_sorting.rs
@@ -8,40 +8,51 @@ use data_structures::sorting::heap_sort::Heap;
 use data_structures::sorting::merge_sort;
 use data_structures::sorting::quick_sort;
 use data_structures::sorting::selection_sort;
+use data_structures::sorting::insertion_sort;
 
 fn main() {
-    // let mut arr = [0i64; 10000000];
     let mut arr = Vec::new();
     let mut rng = thread_rng();
-    for i in 0..10000 {
+    for i in 0..100000 {
         arr.push(rng.gen_range(0, 100));
     }
-    // merge sort
-    //  let before = Instant::now();
-    /// merge_sort::sort(&mut arr);
-    // println!("Elapsed time: {:.1?} for merge sort ", before.elapsed());
 
-    // heap sort
 
-    //let before = Instant::now();
-    // let mut heap = Heap::new(&mut arr);
-    // heap.sort();
-    // println!("Elapsed time: {:.1?} for heap sort ", before.elapsed());
+    //insertion_sort 
+
+        
     let before = Instant::now();
-    let size = arr.len() - 1;
-   // quick_sort::quick_sort(&mut arr, 0, size);
-    quick_sort::quick_sort(&mut arr);
-
-    println!("Elapsed time: {:.1?} in quick sort", before.elapsed());
-
+    let size = arr.len();
+   insertion_sort::sort( &mut arr);
+    println!("Elapsed time: {:.1?} in insertion sort", before.elapsed());
+    
     // selection  sort
     
     let before = Instant::now();
     let size = arr.len();
    selection_sort::sort(&mut arr,size);
-
     println!("Elapsed time: {:.1?} in selection sort", before.elapsed());
 
+    // merge sort
+    let before = Instant::now();
+    merge_sort::sort(&mut arr);
+    println!("Elapsed time: {:.1?} for merge sort ", before.elapsed());
 
-   // println!("array now is {:?}",arr);
+    // heap sort
+
+    let before = Instant::now();
+    let mut heap = Heap::new(&mut arr);
+     heap.sort();
+    println!("Elapsed time: {:.1?} for heap sort ", before.elapsed());
+
+
+    //quick sort 
+
+    let before = Instant::now();
+    let size = arr.len() - 1;
+    quick_sort::quick_sort(&mut arr);
+    println!("Elapsed time: {:.1?} in quick sort", before.elapsed());
+
+
+
 }
--- src/sorting/insertion_sort.rs
+++ src/sorting/insertion_sort.rs
@@ -1,5 +1,5 @@
 //insertion sort in ascending order
-pub fn sort(mut data: Vec<i64>) -> Vec<i64> {
+pub fn sort( data: &mut Vec<i32>)  {
     for j in 1..data.len() {
         let key = data[j];
         let mut i = j;
@@ -8,7 +8,22 @@ pub fn sort(mut data: Vec<i64>) -> Vec<i64> {
             data[i] = data[i - 1];
             i -= 1;
         }
-        data[i + 1] = key;
+        data[i] = key;
     }
-    data
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+   
+
+    #[test]
+    fn test_sort()    {
+        let mut data= vec![2,8,7,1,3,5,6,4];
+       sort(&mut data);
+       
+    assert_eq!(data,vec![1,2,3,4,5,6,7,8]);
+    }
 }
