8590ea2f9a28076d8e07c86e2fdd06bfd1a9403d
Amit Baijal <amit.baijal@publicissapient.com>
2023-03-18 17:39:31 +0530

    subsets

---
+++ recursion/src/Subsets.java
@@ -0,0 +1,22 @@
+public class Subsets {
+    
+
+    private static  void subsets( String soFar, String rest)  {
+        if(rest.equals(""))  {
+            System.out.println(soFar);
+            return;
+        }
+
+        // subsets including first character 
+        subsets(soFar+rest.charAt(0), rest.substring(1));
+        // subsets exceluding first character 
+        subsets(soFar, rest.substring(1));
+
+    }
+
+
+    public static void main(String arg[])   {
+
+           subsets("", "ABCDEF");
+    }
+}
